be3a43cd26769479f80eaecd84a61ce568010cd0
Jhaye Noung <Jhaye@Jhayes-Mac.local>
2024-01-11 12:31:20 +0630

    "building MenuItem API"

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import MenuItemSerializer
+from .models import MenuItem
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
