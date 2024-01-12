bf45421d3eb93a854e5743d3bcd5ce062870bb87
Jhaye Noung <Jhaye@Jhayes-Mac.local>
2024-01-12 16:41:09 +0630

    adding security and test

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework import viewsets
 from .serializers import MenuItemSerializer, BookingSerializer
 from .models import MenuItem, Booking
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
@@ -11,7 +12,7 @@ def index(request):
 class MenuItemView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-
+    permission_classes = [IsAuthenticated]
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = MenuItem.objects.all()
@@ -21,3 +22,4 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 class BookingView(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
